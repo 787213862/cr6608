e3e98522e1f3fdc587553ece1a32d839e2811497
787213862 <42348632+787213862@users.noreply.github.com>
2026-06-06 21:51:24 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -2,7 +2,12 @@
 # 安装 ShadowsocksR Plus+
 git clone https://github.com/fw876/helloworld package/helloworld
 
-# 确保安装 luci-compat 依赖（SSRP 运行必备，否则插件在后台会报错）
+# 确保安装 luci-compat 依赖
 git clone https://github.com/immortalwrt/luci-compat package/luci-compat
 
+# 这里把两行代码放在 exit 0 之前
+echo "CONFIG_PACKAGE_luci-app-ssr-plus=y" >> .config
+echo "CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDE_Hysteria=y" >> .config
+
+# 最后才写 exit 0
 exit 0
