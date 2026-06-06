79b0f959f59e67d85659145afa84c43bd8188497
787213862 <42348632+787213862@users.noreply.github.com>
2026-06-06 21:49:06 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+# 安装 ShadowsocksR Plus+
+git clone https://github.com/fw876/helloworld package/helloworld
+
+# 确保安装 luci-compat 依赖（SSRP 运行必备，否则插件在后台会报错）
+git clone https://github.com/immortalwrt/luci-compat package/luci-compat
+
 exit 0
